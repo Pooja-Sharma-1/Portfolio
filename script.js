59e0c509e8c6f05615a8302aceaace40c15621c4
Pooja Sharma <115809481+Pooja-Sharma-1@users.noreply.github.com>
2024-09-10 21:52:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ***************************typing animation ********************** */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Programmer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
